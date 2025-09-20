6e6ee3d72a3ca70827a29aacbd33f0ec05960ece
Igashu-Randolf <hicaritung@gmail.com>
2025-09-20 17:12:29 +0800

    Add polytropic equation

--- wd_setup.py
+++ wd_setup.py
@@ -1,28 +1,31 @@
 import numpy as np
 
+me = 9.1093837e-31
+Mp = 1.67262192e-27
+
 class WhiteDwarf:
-    def __init__(self, Ye, rhoc_scaled):
+    def __init__(self, Ye, rhoc_scaled, gamma):
         self.Ye = Ye
         self.rhoc_scaled = rhoc_scaled
 
         self.rho0 = 9.79e5 / Ye
         self.R0 = 7.72e8 * Ye
         self.M0 = 5.67e33 * (Ye ** 2) 
+        self.k = Ye * me / Mp
 
-    def _gamma(self, x):
-        return (x ** 2) / (3 * np.sqrt(1 + (x ** 2)))
+        self.GAMMA = gamma
+
+    #def _gamma(self, x):
+    #    return (x ** 2) / (3 * np.sqrt(1 + (x ** 2)))
     
     def _x(self, rhob):
         return (rhob) ** (1/3)
     
     def get_derivative(self, state, rb):
-        rhob = state[0]
-        mb = state[1]
+        # state = [rho, drho/dr]
+        rho = state[0]
+        drhodr = state[1]
 
-        x = self._x(rhob)
-        gamma = self._gamma(x)
-
-        dmdr = rhob * (rb ** 2)
-        drhodr = -(mb * rhob) / (gamma * (rb ** 2))
-
-        return np.array([drhodr, dmdr])
+        d2rhodr2 =  - (2 / rb) * drhodr - (self.GAMMA - 2) * (drhodr ** 2) / rho - rho ** (3 - self.GAMMA)
+        dmdr = rho * (rb ** 2)
+        return np.array([drhodr, d2rhodr2, dmdr])
